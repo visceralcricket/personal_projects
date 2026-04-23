c36522b61c30ae484792f5acdbec8faf397f1003
visceralcricket <felipetapiasiede@gmail.com>
2026-04-22 22:57:29 -0400

    Add files via upload

---
+++ C codes/dataStructures/binaryTree/bst.h
@@ -0,0 +1,17 @@
+#ifndef BST_H
+#define BST_H
+
+typedef struct Node Node;
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+};
+
+Node *createNode(int value);
+Node *insertTree(Node *node, int value);
+
+void freeTree(Node *node);
+void showTree(Node *root);
+
+#endif
