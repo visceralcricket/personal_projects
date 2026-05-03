65a3748fe8919de4359710d3c1e701e88f7302c4
visceralcricket <felipetapiasiede@gmail.com>
2026-05-03 18:40:58 -0400

    refined structures

--- C codes/dataStructures/AVL_tree/avl.c
+++ C codes/dataStructures/AVL_tree/avl.c
@@ -4,13 +4,13 @@
 struct avl_node {
     Pair *pair;
     int height;
+    struct avl_node *parent;
     struct avl_node *left, *right;
 };
 
 struct tree_map {
     avl_node *root;
-    avl_node *parent;
-    avl_node *left, right;
+    avl_node *current;
 }
 
 avl_node createNode(void *key, void *data) {
